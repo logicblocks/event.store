c4313d6d624f3dbb157378f586e748dcef8408f3
Toby Clemson <tobyclemson@gmail.com>
2024-10-10 13:35:06 +0100

    Add build tooling.

--- tests/unit/event/store/adapters/test_in_memory.py
+++ tests/unit/event/store/adapters/test_in_memory.py
@@ -1,12 +1,17 @@
 import unittest
 
-from event.types import Event
+from event.store.types import Event
 from event.store.adapters import InMemoryEventStoreAdapter
 
+
 class TestInMemoryAdapter(unittest.TestCase):
-    def test_saves_single_event(self):
+    def test_publishes_single_event(self):
         adapter = InMemoryEventStoreAdapter()
-        event = Event(name='something-happened', payload={'foo': 'bar'})
+        event = Event(name="something-happened", payload={"foo": "bar"})
         adapter.publish([event])
 
         self.assertEqual(adapter._events, [event])
+
+
+if __name__ == "__main__":
+    unittest.main()

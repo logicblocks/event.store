a7f13eb6ba9ac903df35f85d10ca8eac32f86038
jonassvalin <jonassvalin@gmail.com>
2024-10-24 13:24:27 +0200

    Allow multiple events in save

--- src/logicblocks/event/store/adapters/postgres.py
+++ src/logicblocks/event/store/adapters/postgres.py
@@ -2,13 +2,55 @@ from collections.abc import Set
 from typing import Sequence, Iterator
 from uuid import uuid4
 
-from psycopg import Connection
+from psycopg import Connection, Cursor
 from psycopg.types.json import Jsonb
 from psycopg_pool import ConnectionPool
 
 from logicblocks.event.store.adapters import StorageAdapter
 from logicblocks.event.store.conditions import WriteCondition
 from logicblocks.event.types import NewEvent, StoredEvent
+
+
+def insert_event(
+    cursor: Cursor, stream: str, category: str, event: NewEvent, position: int
+):
+    event_id = uuid4().hex
+    cursor.execute(
+        """
+        INSERT INTO events (
+          id, 
+          name, 
+          stream, 
+          category, 
+          position, 
+          payload, 
+          observed_at, 
+          occurred_at
+      )
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+        """,
+        (
+            event_id,
+            event.name,
+            stream,
+            category,
+            position,
+            Jsonb(event.payload),
+            event.observed_at,
+            event.occurred_at,
+        ),
+    )
+
+    return StoredEvent(
+        id=event_id,
+        name=event.name,
+        stream=stream,
+        category=category,
+        position=position,
+        payload=event.payload,
+        observed_at=event.observed_at,
+        occurred_at=event.occurred_at,
+    )
 
 
 class PostgresStorageAdapter(StorageAdapter):
@@ -23,50 +65,12 @@ class PostgresStorageAdapter(StorageAdapter):
         events: Sequence[NewEvent],
         conditions: Set[WriteCondition] = frozenset(),
     ) -> Sequence[StoredEvent]:
-        event = events[0]
-        event_id = uuid4().hex
-        event_position = 0
-
         with self.connection_pool.connection() as connection:
             with connection.cursor() as cursor:
-                cursor.execute(
-                    """
-                    INSERT INTO events (
-                      id, 
-                      name, 
-                      stream, 
-                      category, 
-                      position, 
-                      payload, 
-                      observed_at, 
-                      occurred_at
-                  )
-                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
-                    """,
-                    (
-                        event_id,
-                        event.name,
-                        stream,
-                        category,
-                        event_position,
-                        Jsonb(event.payload),
-                        event.observed_at,
-                        event.occurred_at,
-                    ),
-                )
-
-        return [
-            StoredEvent(
-                id=event_id,
-                name=event.name,
-                stream=stream,
-                category=category,
-                position=event_position,
-                payload=event.payload,
-                observed_at=event.observed_at,
-                occurred_at=event.occurred_at,
-            )
-        ]
+                return [
+                    insert_event(cursor, stream, category, event, position)
+                    for position, event in enumerate(events)
+                ]
 
     def scan_stream(
         self, *, category: str, stream: str
--- tests/integration/logicblocks/event/store/adapters/test_postgres.py
+++ tests/integration/logicblocks/event/store/adapters/test_postgres.py
@@ -4,8 +4,16 @@ from datetime import datetime
 from psycopg import Connection
 from psycopg_pool import ConnectionPool
 
-from logicblocks.event.types import NewEvent
 from logicblocks.event.store.adapters import PostgresStorageAdapter
+
+from logicblocks.event.testing import NewEventBuilder
+
+from logicblocks.event.testing.data import (
+    random_event_name,
+    random_event_payload,
+    random_event_stream_name,
+    random_event_category_name,
+)
 
 
 def conninfo(
@@ -33,18 +41,20 @@ class TestPostgresStorageAdapter(unittest.TestCase):
     def test_stores_single_event_for_later_retrieval(self):
         adapter = PostgresStorageAdapter(connection_pool=self.pool)
 
-        event_name = "something-happened"
-        event_category = "some-category"
-        event_stream = "some-stream"
-        event_payload = {"foo": "bar"}
+        event_name = random_event_name()
+        event_category = random_event_category_name()
+        event_stream = random_event_stream_name()
+        event_payload = random_event_payload()
         event_observed_at = datetime.now()
         event_occurred_at = datetime.now()
 
-        new_event = NewEvent(
-            name=event_name,
-            payload=event_payload,
-            observed_at=event_observed_at,
-            occurred_at=event_occurred_at,
+        new_event = (
+            NewEventBuilder()
+            .with_name(event_name)
+            .with_payload(event_payload)
+            .with_observed_at(event_observed_at)
+            .with_occurred_at(event_occurred_at)
+            .build()
         )
 
         stored_events = adapter.save(
@@ -70,6 +80,86 @@ class TestPostgresStorageAdapter(unittest.TestCase):
                             event_observed_at,
                             event_occurred_at,
                         )
+                    ],
+                )
+
+    def test_stores_multiple_events_for_later_retrieval(self):
+        adapter = PostgresStorageAdapter(connection_pool=self.pool)
+
+        event_name_1 = random_event_name()
+        event_category_1 = random_event_category_name()
+        event_stream_1 = random_event_stream_name()
+        event_payload_1 = random_event_payload()
+        event_observed_at_1 = datetime.now()
+        event_occurred_at_1 = datetime.now()
+
+        new_event_1 = (
+            NewEventBuilder()
+            .with_name(event_name_1)
+            .with_payload(event_payload_1)
+            .with_observed_at(event_observed_at_1)
+            .with_occurred_at(event_occurred_at_1)
+            .build()
+        )
+
+        stored_events_1 = adapter.save(
+            category=event_category_1,
+            stream=event_stream_1,
+            events=[new_event_1],
+        )
+        stored_event_1 = stored_events_1[0]
+
+        event_name_2 = random_event_name()
+        event_category_2 = random_event_category_name()
+        event_stream_2 = random_event_stream_name()
+        event_payload_2 = random_event_payload()
+        event_observed_at_2 = datetime.now()
+        event_occurred_at_2 = datetime.now()
+
+        new_event_2 = (
+            NewEventBuilder()
+            .with_name(event_name_2)
+            .with_payload(event_payload_2)
+            .with_observed_at(event_observed_at_2)
+            .with_occurred_at(event_occurred_at_2)
+            .build()
+        )
+
+        stored_events_2 = adapter.save(
+            category=event_category_2,
+            stream=event_stream_2,
+            events=[new_event_2],
+        )
+        stored_event_2 = stored_events_2[0]
+
+        with self.pool.connection() as connection:
+            with connection.cursor() as cursor:
+                cursor.execute("SELECT * FROM events")
+                records = cursor.fetchall()
+
+                self.assertEqual(
+                    records,
+                    [
+                        (
+                            stored_event_1.id,
+                            event_name_1,
+                            event_stream_1,
+                            event_category_1,
+                            0,
+                            event_payload_1,
+                            event_observed_at_1,
+                            event_occurred_at_1,
+                        ),
+                        (
+                            stored_event_2.id,
+                            event_name_2,
+                            event_stream_2,
+                            event_category_2,
+                            0,
+                            event_payload_2,
+                            event_observed_at_2,
+                            event_occurred_at_2,
+                        ),
                     ],
                 )
 

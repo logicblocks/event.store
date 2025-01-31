c58e39622f840a70146cb6ea1a65d0becdf404da
Toby Clemson <tobyclemson@gmail.com>
2025-01-31 12:44:10 +0000

    Add notes to postgres lock implementation.

--- src/logicblocks/event/processing/broker/locks/postgres.py
+++ src/logicblocks/event/processing/broker/locks/postgres.py
@@ -12,3 +12,12 @@ class PostgresLockManager(LockManager):
     @asynccontextmanager
     def wait_for_lock(self, lock_name: str) -> AsyncGenerator[Lock, None]:
         raise NotImplementedError()
+
+    # wait for lock -> pg_advisory_xact_lock
+    #               -> pg_advisory_lock
+    # try lock -> pg_try_advisory_xact_lock
+    #          -> pg_try_advisory_lock
+    # not sure which to use
+    #
+    # should the postgres lock manager manage its own dedicated connection?
+    # how long running can this connection be?

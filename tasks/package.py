ff89720112f703f287090c0cec41dcda8dd86587
Toby Clemson <tobyclemson@gmail.com>
2025-11-26 20:50:53 +0000

    Fix package tasks.

--- tasks/package.py
+++ tasks/package.py
@@ -1,8 +1,10 @@
+import os
+
 from invoke import task, Context
 
 @task
 def publish(context: Context) -> None:
     """Publish library to pypi."""
-    context.run(f"poetry publish --build {settings.url}", env={
+    context.run(f"poetry publish --build", env={
         "POETRY_PYPI_TOKEN_PYPI": os.getenv("PYPI_API_KEY"),
     })

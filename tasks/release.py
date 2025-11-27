b057da4421b18173f08d51d84e3d6d32c6e6f469
InfraBlocks CI <ci@infrablocks.com>
2025-11-27 00:38:45 +0000

    Build before releasing.

--- tasks/release.py
+++ tasks/release.py
@@ -1,12 +1,13 @@
 from invoke import Context, task
 
-from . import package, git, version
+from . import package, git, version, build
 
 
 @task
 def prerelease(context: Context):
     """Release prerelease version of library."""
     git.set_author(context)
+    build.build(context)
     package.publish(context)
     git.tag_version(context)
     version.bump_and_push(context, type=["alpha"])
@@ -17,6 +18,7 @@ def release(context: Context):
     """Release release version of library."""
     git.set_author(context)
     version.bump_and_push(context, type=["patch"])
+    build.build(context)
     package.publish(context)
     git.tag_version(context)
-    version.bump_and_push(context, type=["patch", "alpha"])
+    version.bump_and_push(context, type="prepatch")

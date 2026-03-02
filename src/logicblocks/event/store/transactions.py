c35155392207bf9fba379d7f729086cd6f6f28d5
Corin Chaplin <corin@go-atomic.io>
2026-03-02 10:41:23 +0000

    Update `event_store_transaction` return type to be a Coroutine instead of Awatable

--- src/logicblocks/event/store/transactions.py
+++ src/logicblocks/event/store/transactions.py
@@ -1,8 +1,7 @@
 from abc import ABC, abstractmethod
+from collections.abc import Awaitable, Callable, Coroutine
 from functools import wraps
 from typing import (
-    Awaitable,
-    Callable,
     ClassVar,
     Final,
     Never,
@@ -15,7 +14,7 @@ class event_store_transaction[AdditionalReturn = Never](ABC):
     def __call__[**P, R](
         self,
         handler: Callable[P, Awaitable[R]],
-    ) -> Callable[P, Awaitable[AdditionalReturn | R]]:
+    ) -> Callable[P, Coroutine[None, None, AdditionalReturn | R]]:
         @wraps(handler)
         async def _wrapper(
             *args: P.args, **kwargs: P.kwargs

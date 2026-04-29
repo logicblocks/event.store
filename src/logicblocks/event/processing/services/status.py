bb20e3098fda184df4be86d16aa9ccfdfafffa78
Corin Chaplin <corin@go-atomic.io>
2026-04-29 17:19:01 +0100

    Add StatusTrackingService and HasProcessStatus protocol

---
+++ src/logicblocks/event/processing/services/status.py
@@ -0,0 +1,32 @@
+import asyncio
+from typing import Any
+
+from ..process.base import ProcessStatus
+from .types import Service
+
+
+class StatusTrackingService[T = Any](Service[T]):
+    def __init__(self, service: Service[T]):
+        self._service = service
+        self._status = ProcessStatus.INITIALISED
+
+    @property
+    def status(self) -> ProcessStatus:
+        return self._status
+
+    async def execute(self) -> T:
+        try:
+            self._status = ProcessStatus.RUNNING
+            result = await self._service.execute()
+            self._status = ProcessStatus.STOPPED
+            return result
+        except asyncio.CancelledError:
+            self._status = ProcessStatus.STOPPED
+            raise
+        except BaseException:
+            self._status = ProcessStatus.ERRORED
+            raise
+
+    @property
+    def name(self) -> str:
+        return self._service.name

88736dfe814f89cfba64a8668beb597d4aa293da
Corin <corin@go-atomic.io>
2026-07-30 12:46:15 +0100

    WIP2: Add automatic tracing for stored events

--- src/logicblocks/event/store/tracing.py
+++ src/logicblocks/event/store/tracing.py
@@ -1,13 +1,45 @@
+from abc import ABC
+from collections.abc import Callable
 from contextlib import contextmanager
 from contextvars import ContextVar
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict
+from typing import Any, Self
 from uuid import uuid4
 
+from logicblocks.event.types import JsonValue, is_json_object
 
-@dataclass
+
+@dataclass(frozen=True, kw_only=True)
 class TracingMetadata:
     trace_id: str
     causation_event_id: str | None
+
+    def serialise(self, fallback: Callable[[object], JsonValue]) -> JsonValue:
+        return asdict(self)
+
+    @classmethod
+    def deserialise(
+            cls, value: JsonValue, fallback: Callable[[type[Any], JsonValue], Any]
+    ) -> Self:
+        if is_json_object(value):
+            trace_id = value.get("trace_id")
+            causation_event_id = value.get("causation_event_id")
+            if isinstance(trace_id, str) and isinstance(causation_event_id, (str, None)):
+                return cls(
+                    trace_id=trace_id,
+                    causation_event_id=causation_event_id,
+                )
+
+        return fallback(cls, value)
+
+
+class TracingMixin(ABC):
+    tracing: TracingMetadata | None
+
+    def with_tracing(self):
+        tracing = get_tracing_metadata()
+
+
 
 
 _tracing_metadata_var = ContextVar[TracingMetadata | None](
